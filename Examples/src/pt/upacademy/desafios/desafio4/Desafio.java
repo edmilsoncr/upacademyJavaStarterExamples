8614e2fadba3fb064f38b510735f61e4b0049434
Edmilson <edmilsoncrodrigues@gmail.com>
2019-12-12 16:37:48 +0000

    Adicionado a Solucao 2 para o desafio 4

--- Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
@@ -6,16 +6,30 @@ import java.util.Scanner;
 
 public class Desafio {
 
+//	public static int[] ints(String string) {
+//		List<Integer> numbers = new ArrayList<Integer>();
+//		String stringArray[] = string.split(" ");
+//		for (String stringItem : stringArray) {
+//			Scanner sc = new Scanner(stringItem);
+//			if (sc.hasNextInt()) {
+//				numbers.add(sc.nextInt());
+//			}
+//			sc.close();
+//		}
+//		return numbers.stream().mapToInt(i -> i).toArray();
+//	}
+	
 	public static int[] ints(String string) {
 		List<Integer> numbers = new ArrayList<Integer>();
-		String stringArray[] = string.split(" ");
-		for (String stringItem : stringArray) {
-			Scanner sc = new Scanner(stringItem);
+		Scanner sc = new Scanner(string);
+		while (sc.hasNext()) {
 			if (sc.hasNextInt()) {
 				numbers.add(sc.nextInt());
+			} else {
+				sc.next();
 			}
-			sc.close();
 		}
+		sc.close();
 		return numbers.stream().mapToInt(i -> i).toArray();
 	}
 }
