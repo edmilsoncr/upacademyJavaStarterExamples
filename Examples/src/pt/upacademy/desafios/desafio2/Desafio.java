5bccf76c154798f833fb8f318ec2261cf464c0ea
Edmilson <edmilsoncrodrigues@gmail.com>
2019-12-20 14:04:45 +0000

    Adicionado solucao 2 para o desafio 2

--- Examples/src/pt/upacademy/desafios/desafio2/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio2/Desafio.java
@@ -1,25 +1,34 @@
 package pt.upacademy.desafios.desafio2;
 
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Desafio {
 
 	public static String order(String string) {
-		if (string.isEmpty()) {
-			return "";
-		}
+//		if (string.isEmpty()) {
+//			return "";
+//		}
+//		String stringArray[] = string.split(" ");
+//		String result = "";
+//		for (int i = 0; i <= 9; i++) {
+//			String number = Integer.toString(i);
+//			for (int j = 0; j < stringArray.length; j++) {
+//				if (stringArray[j].contains(number)) {
+//					result += stringArray[j] + " ";
+//				}
+//			}
+//		}
+//		result = result.substring(0, result.length() - 1);
+//		System.out.println(result);
+//		return result;
+		
 		String stringArray[] = string.split(" ");
-		String result = "";
-		for (int i = 0; i <= 9; i++) {
-			String number = Integer.toString(i);
-			for (int j = 0; j < stringArray.length; j++) {
-				if (stringArray[j].contains(number)) {
-					result += stringArray[j] + " ";
-				}
-			}
-		}
-		result = result.substring(0, result.length() - 1);
-		System.out.println(result);
-		return result;
+
+		return Stream.of(stringArray)
+		.sorted((str1, str2) -> Integer.compare(new Scanner(str1).useDelimiter("\\D+").nextInt(), new Scanner(str2).useDelimiter("\\D+").nextInt()))
+		.collect(Collectors.joining(" "));
 	}
 }
